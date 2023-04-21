048ff856a7e81ef5c95844bc1cace8e8f3a8eb5a
Michael Collins <mcollins21@luc.edu>
2023-04-21 15:57:56 -0500

    Place audio sample in "AUDIOSAMPLE.wav"

--- src/audioPlayer.java
+++ src/audioPlayer.java
@@ -7,11 +7,11 @@ import javax.sound.sampled.Clip;
 public class audioPlayer {
     public static void main(String[] args) {
         try {
-            File audioFile = new File("audio.wav");
-            Clip clip = AudioSystem.getClip();
-            clip.open(AudioSystem.getAudioInputStream(audioFile));
-            clip.start();
-            Thread.sleep(clip.getMicrosecondLength() / 1000);
+            File audioFile = new File("AUDIOSAMPLE.wav");
+            Clip audiofile = AudioSystem.getClip();
+            audiofile.open(AudioSystem.getAudioInputStream(audioFile));
+            audiofile.start();
+            Thread.sleep(audiofile.getMicrosecondLength() / 1000);
         } catch (Exception e) {
             System.err.println("Error playing audio: " + e.getMessage());
         }
